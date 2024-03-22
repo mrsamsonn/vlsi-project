da2e179bfb561a5995d153d91cecb7313a38118f
John San Juan <98930957+mrsamsonn@users.noreply.github.com>
2024-03-22 07:39:11 -0700

    magic files fix/update - 2

---
+++ magic/4OR.cmd
@@ -0,0 +1,50 @@
+h VDD
+l VSS
+analyzer A B C D Y
+l A B C D
+s
+l A B C
+h D
+s
+l A B D
+h C
+s
+l A B
+h C D
+s
+l A C D
+h B
+s
+l A C
+h B D
+s
+l A D
+h B C
+s
+l A
+h B C D
+s
+h A
+l B C D
+s
+h A D
+l B C
+s
+h A C
+l B D
+s
+h A C D
+l B
+s
+h A B
+l C D
+s
+h A B D
+l C
+s
+h A B C
+l D
+s
+h A B C D
+s
+
